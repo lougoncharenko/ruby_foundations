a1a00f40b2906661e4932039b344be68435eb503
Louisa Goncharenko <louisa.goncharenko@shopify.com>
2023-01-30 09:07:40 -0500

    first commit

---
+++ main.rb
@@ -0,0 +1,10 @@
+# variables
+my_num = 25
+my_string = 'ruby rocks!'
+
+puts my_num
+puts my_string
+# puts prints things on a new line
+print "giraffe"
+# print prints things on the same line
+puts "Louisa"
