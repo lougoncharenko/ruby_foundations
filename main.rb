d175ebb6da44b1d42460fe3cbdb9ff5ef705de11
Louisa Goncharenko <louisa.goncharenko@shopify.com>
2023-01-30 09:33:58 -0500

    data types

--- main.rb
+++ main.rb
@@ -1,20 +1,17 @@
-# variables
-my_num = 25
-my_string = 'ruby rocks!'
+# How to print to the console
 
-puts my_num
-puts my_string
 # puts prints things on a new line
 print "giraffe"
 # print prints things on the same line
 puts "Louisa"
 
-# print a shape
+# Print a shape
 puts "   /|"
 puts "  / |"
 puts " /  |"
 puts "/___|"
 
+# using variables
 character_name = "John"
 character_age = "35"
 
@@ -22,3 +19,14 @@ puts ("There once was a a man named " + character_name)
 puts ("he was " + character_age + " years old")
 puts ("He really liked the name " + character_name)
 puts ("but didn't like being " + character_age)
+
+# data types
+my_num = 25
+my_string = 'ruby rocks!'
+my_boolean = true
+my_float = 3.4
+
+puts my_num
+puts my_string
+puts my_boolean
+puts my_float
